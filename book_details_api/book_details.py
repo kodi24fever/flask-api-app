d9b1ba782c4a93a11dfea4b0b5dba5e24612d789
Gabriel Hurtado <102824698+Ghurt007@users.noreply.github.com>
2023-07-23 21:25:59 -0400

    Update book_details.py

--- book_details_api/book_details.py
+++ book_details_api/book_details.py
@@ -1,22 +1,14 @@
 from flask import Blueprint, render_template, request, jsonify
-from database import db, BookDetails
+from database import db, BookDetails, AuthorDetails
 
 
 
 book_details = Blueprint('book_details', __name__)
 
 
-class ProductSchema():
-    class Meta:
-        details = ('book_name', 'ISBN', 'book_description', 'book_price',
-                   'author', 'genre', 'year_published', 'copies_sold')
 
 
-product_schema = ProductSchema()
-
-@book_details.route("/book_details", methods=['POST']) # api name
-
-
+@book_details.route("/admin/create_book_details", methods=['POST']) # api name
 # sample function to create an endpoint which should return a json format
 # Creates book with it's details
 def add_book_Details():
@@ -30,6 +22,8 @@ def add_book_Details():
     copies_sold = request.json['copies_sold']
 
     new_book_Details = BookDetails(
+
+
             book_name = book_name,
             ISBN = ISBN,
             book_description = book_description,
@@ -51,6 +45,8 @@ def get_details_ISBN(ISBN):
     if find_by_ISBN:
         serialized_book_details = [
             {
+
+
               'book_name': add_book_Details.book_name,
               'ISBN': add_book_Details.ISBN,
               'book_description': add_book_Details.book_description,
@@ -65,30 +61,58 @@ def get_details_ISBN(ISBN):
 
         ]
 
-        return render_template('details.html', get_details_ISBN= serialized_book_details)
+        return render_template('ISBN_details.html', get_details_ISBN= serialized_book_details)
 
     else:
         return jsonify(message = 'No book was found with this ISBN')
 
 
 
+@book_details.route("/admin/create_author", methods=['POST'])
+def create_author():
+
+
+    author_fn = request.json['author_fn']
+    author_ln = request.json['author_ln']
+    biography = request.json['biography']
+    publisher = request.json['publisher']
+
+
+    new_author = AuthorDetails(
+
+        author_fn = author_fn,
+        author_ln = author_ln,
+        biography = biography,
+        publisher = publisher,
+
+    )
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return jsonify({'message': 'New Author created successfully'})
+
+
+@book_details.route("/search_by_author/<author>", methods=['GET'])
+def get_details_author(author):
+    search_author = BookDetails.query.filter_by(author = author).all()
+    if search_author:
+
+        authors_book_details = [
+            {
+
+
+                'book_name': add_book_Details.book_name,
+
+            }
+            for add_book_Details in search_author
+
+        ]
+
+        return render_template('author_details.html', get_details_author=authors_book_details)
+
+    else:
+        return jsonify(message='No book was found for this author')
 
 
 
-    #details = BookDetails.query.all()
-
-    #for book in details:
-        #print("Book's Title: " + book.book_name)
-        #print("ISBN: " + str(book.ISBN))
-        #print("Book's Description: " + book.book_description)
-        #print("Price: $" + str(book.book_price))
-        #print("Author: " + book.author)
-        #print("Genre: " + book.genre)
-        #print("Year of Publication: " + str(book.year_published))
-        #print("Copies Sold: " + str(book.copies_sold))
-
-
-
-
-
-     #return render_template("book_details_sample.html", title="sample", details=details)
